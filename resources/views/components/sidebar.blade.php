2a58c13f7a7349cb15ec0e741cf42985324ca7a7
Marcos Messias <57878494+marcosmessias-src@users.noreply.github.com>
2022-07-24 11:12:53 -0300

    Arquivo de Sidebar do App.

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,45 @@
+<!--begin::Aside-->
+<div id="kt_aside" class="aside bg-primary" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="auto" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_toggle">
+    <!--begin::Logo-->
+    <div class="aside-logo d-none d-lg-flex flex-column align-items-center flex-column-auto py-8" id="kt_aside_logo">
+        <a href="/metronic8/demo4/../demo4/index.html">
+            <img alt="Logo" src="/metronic8/demo4/assets/media/logos/demo4.svg" class="h-55px" />
+        </a>
+    </div>
+    <!--end::Logo-->
+    <!--begin::Nav-->
+    <div class="aside-nav d-flex flex-column align-lg-center flex-column-fluid w-100 pt-5 pt-lg-0" id="kt_aside_nav">
+        <!--begin::Primary menu-->
+        <div class="hover-scroll-overlay-y my-2 my-lg-5" id="kt_aside_menu_wrapper" data-kt-scroll="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer" data-kt-scroll-wrappers="#kt_aside, #kt_aside_menu" data-kt-scroll-offset="5px">
+            <!--begin::Menu-->
+            <div id="kt_aside_menu" class="menu menu-column menu-title-gray-600 menu-state-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500 fw-semibold fs-6" data-kt-menu="true">
+                <!--begin:Menu item-->
+                <div data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="right-start" class="menu-item py-2">
+                    <!--begin:Menu link-->
+                    <span class="menu-link menu-center">
+                        <span class="menu-icon me-0">
+                            <i class="fonticon-house fs-1"></i>
+                        </span>
+                    </span>
+                    <!--end:Menu link-->
+                </div>
+                <!--end:Menu item-->
+            </div>
+            <!--end::Menu-->
+        </div>
+        <!--end::Primary menu-->
+    </div>
+    <!--end::Nav-->
+    <!--begin::Footer-->
+    <div class="aside-footer d-flex flex-column align-items-center flex-column-auto" id="kt_aside_footer">
+        <!--begin::Menu-->
+        <div class="mb-7">
+            <button type="button" class="btn btm-sm btn-custom btn-icon" data-kt-menu-trigger="click" data-kt-menu-overflow="true" data-kt-menu-placement="top-start" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-dismiss="click" title="Sair">
+                <i class="fonticon-notification fs-1"></i>
+            </button>
+        </div>
+        <!--end::Menu-->
+    </div>
+    <!--end::Footer-->
+</div>
+<!--end::Aside-->
