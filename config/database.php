7837d95fdd9a15e9f59a83663d2d9c79ede85ec0
Marcos Messias <57878494+marcosmessias-src@users.noreply.github.com>
2022-07-24 16:24:05 -0300

    Instalado pacotes para o MongoDB!

--- config/database.php
+++ config/database.php
@@ -43,6 +43,12 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb+srv://username:password@<atlas-cluster-uri>/myappdb?retryWrites=true&w=majority'),
+            'database' => 'myappdb',
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
