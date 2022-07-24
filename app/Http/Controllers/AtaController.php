499c59100d8fa65769aaba945b8dab2b6f1abf7e
Marcos Messias <57878494+marcosmessias-src@users.noreply.github.com>
2022-07-24 15:46:45 -0300

    Model/Migration/Controller ATA

---
+++ app/Http/Controllers/AtaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ata;
+use Illuminate\Http\Request;
+
+class AtaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ata  $ata
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ata $ata)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ata  $ata
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ata $ata)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ata  $ata
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ata $ata)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ata  $ata
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ata $ata)
+    {
+        //
+    }
+}
